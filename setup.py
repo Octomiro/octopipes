46be7fb271e60bf03f3d5784d5a02d1f0f24e9d7
zeddo123 <m.drissi@protonmail.com>
2024-01-09 01:11:36 +0100

    Add Readme.md file to setup.py
    This way it gets pick up when publishing on Pypi

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
+import pathlib
 from setuptools import find_packages
 from setuptools import setup
 
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setup(
     name='octopipes',
@@ -8,6 +14,9 @@ setup(
     description='Pipeline library for AI workflows.',
     author='Octomiro',
     author_email='contact@octomiro.ai',
+    url='https://github.com/octomiro/octopipes',
+    long_description=README,
+    long_description_content_type='text/markdown',
     packages=find_packages(exclude=('tests*', 'testing*')),
     install_requires=[
         'pydantic',
@@ -18,5 +27,9 @@ setup(
     ],
     extras_require={
         'opencv': ['opencv-python']
-    }
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License'
+    ]
 )
