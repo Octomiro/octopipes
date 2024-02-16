af28c516e247d0e53f9c6de0afeac06a9225daf0
zeddo123 <m.drissi@protonmail.com>
2024-02-17 00:28:55 +0100

    Add `requires` flag for the `add` method of Workflows

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -12,3 +12,27 @@ def test_workflow():
 
     wf_iter.recap()
     wf_iter.freeze()
+
+
+def test_process_requires():
+    wf = Workflow('test_wf_1')\
+            .add(lambda x: x + 1)\
+            .add(lambda x, y: x - y, requires='0')
+
+    wf_iter = wf(1)
+    for _ in wf_iter:
+        pass
+    
+    assert wf_iter.outputs[-1] == 1
+        
+
+    wf = Workflow('test_wf_1')\
+            .add(lambda x: x + 'step1')\
+            .add(lambda x: x + 'step2')\
+            .add(lambda x, y, z: x + y + z, requires='0,1')
+
+    wf_iter = wf('input')
+    for _ in wf_iter:
+        pass
+    
+    assert wf_iter.outputs[-1] == 'inputstep1step2inputinputstep1'
