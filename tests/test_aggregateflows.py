399fa466f85e683f0d2642c0d25b918b081ec884
Mohieddine Drissi <m.drissi@protonmail.com>
2024-03-21 21:07:20 +0100

    Add tests for InputWithDeps

--- tests/test_aggregateflows.py
+++ tests/test_aggregateflows.py
@@ -9,7 +9,23 @@ def test_aggregateflows():
             .add(lambda x: x * 2)
     flows = AggregateFlows(2, workflows=[wf1, wf2])
     flows.run_workflows()
+
     assert len(flows.results) == 2
     assert flows.results[0].output == 3
     assert flows.results[1].output == 4
+
+
+def test_aggregateflows_with_dependencies():
+    wf1 = Workflow('test_wf_1')\
+            .add(lambda x, y: x + y, requires='d0')
+
+    wf2 = Workflow('test_wf_2')\
+            .add(lambda x, y: x - y, requires='d0')
+
+    flows = AggregateFlows(2, dependencies=[2], workflows=[wf1, wf2])
+    flows.run_workflows()
+
+    assert len(flows.results) == 2
+    assert flows.results[0].output == 4
+    assert flows.results[1].output == 0
 
