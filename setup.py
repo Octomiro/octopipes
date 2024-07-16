2e47b9d4d4e358cd2b69284a14354517334051d8
Mohieddine Drissi <m.drissi@protonmail.com>
2024-07-16 15:46:27 +0200

    bump version to 0.2.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='octopipes',
-    version='0.2.3',
+    version='0.2.4',
     description='Pipeline library for AI workflows.',
     author='Octomiro',
     author_email='contact@octomiro.ai',
