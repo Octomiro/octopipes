c95cc7b9dcb301c46fc6712fad757c1f4092238a
thamerkhamassi <thamerkhamassi07@gmail.com>
2024-10-11 09:56:49 +0100

    Fix overflow issue in circles to bboxes

--- octopipes/process.py
+++ octopipes/process.py
@@ -1,5 +1,5 @@
 """General useful set of processes"""
 
 def circles_to_bboxes(circles) -> list:
-    return [(int(x - r), int(y + r), int(x + r), int(y - r))
-                for x, y, r in circles]
+    return [(int(x) - int(r), int(y) - int(r), int(x) + int(r), int(y) + int(r)) 
+            for x, y, r in circles]
