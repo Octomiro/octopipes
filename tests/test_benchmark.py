b6ec733527f18082b4dff6e708cc79b33212ab37
hadilaff <affeshadil1@gmail.com>
2025-04-02 13:28:50 +0100

    add tests for each mode

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -1,18 +1,19 @@
 from octopipes.benchmark import Benchmark
 from octopipes.dataset import Dataloader, Dataset, InputWithDeps
 from octopipes.workflow import Workflow
+import pytest
 
 from tests.test_dataset import MockDataset
 
-
-def test_benchmark():
+@pytest.mark.parametrize("mode", ["single","threaded"])
+def test_benchmark(mode):
     wf1 = Workflow('test_wf_1')\
             .add(lambda x: x + 1)
     wf2 = Workflow('test_wf_2')\
             .add(lambda x: x * 2)
     dataset: Dataset = MockDataset([1, 2, 3, 4, 5])
     dataloader = Dataloader(dataset=dataset, batch_size=2, drop_last_batch=True)
-    benchmark = Benchmark(dataloader=dataloader, workflows=[wf1, wf2])
+    benchmark = Benchmark(dataloader=dataloader, workflows=[wf1, wf2],mode=mode)
     benchmark.run_tests()
     assert len(benchmark.results) == 4
     assert benchmark.results[0].results[0].output == 2
@@ -20,8 +21,8 @@ def test_benchmark():
     assert benchmark.results[1].results[0].output == 3
     assert benchmark.results[1].results[1].output == 4
 
-
-def test_benchmark_with_dependencies():
+@pytest.mark.parametrize("mode", ["single","threaded"])
+def test_benchmark_with_dependencies(mode):
     wf1 = Workflow('test_wf_1')\
             .add(lambda x, y: x + y, requires='d0')
 
@@ -31,7 +32,7 @@ def test_benchmark_with_dependencies():
     # This dataset defines inputs with dependencies without ground truth
     dataset: Dataset = MockDataset([InputWithDeps(2, dependencies=[2]), InputWithDeps(1, dependencies=[2])])
     dataloader = Dataloader(dataset=dataset, batch_size=2, drop_last_batch=True)
-    benchmark = Benchmark(dataloader=dataloader, workflows=[wf1, wf2])
+    benchmark = Benchmark(dataloader=dataloader, workflows=[wf1, wf2],mode=mode)
     benchmark.run_tests()
     assert len(benchmark.results) == 2
     assert benchmark.results[0].results[0].output == 4
@@ -45,7 +46,7 @@ def test_benchmark_with_dependencies():
         (InputWithDeps(1, dependencies=[2]), 24)
     ])
     dataloader = Dataloader(dataset=dataset, batch_size=2, drop_last_batch=True)
-    benchmark = Benchmark(dataloader=dataloader, workflows=[wf1, wf2])
+    benchmark = Benchmark(dataloader=dataloader, workflows=[wf1, wf2], mode=mode)
     benchmark.run_tests()
     assert len(benchmark.results) == 2
     assert benchmark.results[0].results[0].output == 4
