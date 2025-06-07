c35d2a33975eb6d9690ca67341030f1f663b74c3
hadilaff <affeshadil1@gmail.com>
2025-06-07 22:41:43 +0100

    fix with enum

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -1,16 +1,14 @@
-from octopipes.benchmark import Benchmark
+from octopipes.benchmark import Benchmark, BenchmarkMode
 from octopipes.dataset import Dataloader, Dataset, InputWithDeps
 from octopipes.workflow import Workflow
 import pytest
 
 from tests.test_dataset import MockDataset
 
-@pytest.mark.parametrize("mode", ["single","threaded"])
+@pytest.mark.parametrize("mode", [BenchmarkMode.SINGLE, BenchmarkMode.THREADED])
 def test_benchmark(mode):
-    wf1 = Workflow('test_wf_1')\
-            .add(lambda x: x + 1)
-    wf2 = Workflow('test_wf_2')\
-            .add(lambda x: x * 2)
+    wf1 = Workflow('test_wf_1').add(lambda x: x + 1)
+    wf2 = Workflow('test_wf_2').add(lambda x: x * 2)
     dataset: Dataset = MockDataset([1, 2, 3, 4, 5])
     dataloader = Dataloader(dataset=dataset, batch_size=2, drop_last_batch=True)
     benchmark = Benchmark(dataloader=dataloader, workflows=[wf1, wf2],mode=mode)
@@ -23,16 +21,14 @@ def test_benchmark(mode):
     assert sorted_result[1].results[0].output == 3
     assert sorted_result[1].results[1].output == 4
 
-@pytest.mark.parametrize("mode", ["single","threaded"])
+@pytest.mark.parametrize("mode", [BenchmarkMode.SINGLE, BenchmarkMode.THREADED])
 def test_benchmark_with_dependencies(mode):
-    wf1 = Workflow('test_wf_1')\
-            .add(lambda x, y: x + y, requires='d0')
+    wf1 = Workflow('test_wf_1').add(lambda x, y: x + y, requires='d0')
 
-    wf2 = Workflow('test_wf_2')\
-            .add(lambda x, y: x - y, requires='d0')
+    wf2 = Workflow('test_wf_2').add(lambda x, y: x - y, requires='d0')
 
     # This dataset defines inputs with dependencies without ground truth
-    dataset: Dataset = MockDataset([InputWithDeps(2, dependencies=[2]), InputWithDeps(1, dependencies=[2])])
+    dataset = MockDataset([InputWithDeps(2, dependencies=[2]), InputWithDeps(1, dependencies=[2])])
     dataloader = Dataloader(dataset=dataset, batch_size=2, drop_last_batch=True)
     benchmark = Benchmark(dataloader=dataloader, workflows=[wf1, wf2],mode=mode)
     benchmark.run_tests()
@@ -43,7 +39,7 @@ def test_benchmark_with_dependencies(mode):
     assert benchmark.results[1].results[1].output == -1
 
     # this dataset defines inputs with dependencies with a ground truth value
-    dataset: Dataset = MockDataset([
+    dataset = MockDataset([
         (InputWithDeps(2, dependencies=[2]), 24),
         (InputWithDeps(1, dependencies=[2]), 24)
     ])
@@ -65,7 +61,7 @@ def test_benchmark_auto_mode_selection():
     # Case 1: batch_size = 1 → auto selects "single"
     dataloader = Dataloader(dataset=dataset, batch_size=1)
     benchmark = Benchmark(dataloader=dataloader, workflows=[wf])
-    assert benchmark.mode == "single"
+    assert benchmark.mode == BenchmarkMode.SINGLE
     benchmark.run_tests()
     assert len(benchmark.results) == 2
     assert benchmark.results[0].results[0].output == 2
@@ -73,6 +69,6 @@ def test_benchmark_auto_mode_selection():
     # Case 2: batch_size = 2 → auto selects "threaded"
     dataloader = Dataloader(dataset=dataset, batch_size=2)
     benchmark = Benchmark(dataloader=dataloader, workflows=[wf])
-    assert benchmark.mode == "threaded"
+    assert benchmark.mode == BenchmarkMode.THREADED
     benchmark.run_tests()
     assert len(benchmark.results) == 2
